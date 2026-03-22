8fbdf46814c10b980bc626160cb78ab1ee9b5b2e
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2026-03-22 21:07:52 +0200

    fix(helpers): define missing redirect function

--- helpers/functions.php
+++ helpers/functions.php
@@ -7,3 +7,8 @@ function e(?string $string): string
     return htmlspecialchars((string) ($string ?? ''), ENT_QUOTES, 'UTF-8');
 }
 
+function redirect(string $url): void
+{
+    header('Location: ' . $url);
+    exit;
+}
